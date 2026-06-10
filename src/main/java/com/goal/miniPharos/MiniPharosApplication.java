1d214b1d5911ae8dcb67126a64920c0349d59c95
Dizo016 <diegossilva2008@icloud.com>
2026-06-10 18:51:30 -0300

    first commit

---
+++ src/main/java/com/goal/miniPharos/MiniPharosApplication.java
@@ -0,0 +1,13 @@
+package com.goal.miniPharos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPharosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPharosApplication.class, args);
+	}
+
+}
